2721e1bbccd3c12590e564695873924a8c432c10
MakrandMZare <66860012+MakrandMZare@users.noreply.github.com>
2022-11-18 11:37:05 +0530

    Initial commit

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    mobile: { type: String, required: true, unique: true },
+    email: { type: String, reequired: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+},
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
---
+++ routes/user.js
@@ -0,0 +1,60 @@
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+
+const router = require("express").Router();
+
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString();
+    }
+    try {
+        const updatedUser = User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted...");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//GET USER
+router.get("/find:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//GET USER
+router.get("/", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
